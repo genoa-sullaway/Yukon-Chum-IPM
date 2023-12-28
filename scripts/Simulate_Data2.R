89c439231e53f823cf334d02a4c7545c7335f52a
Genoa Sullaway <42249501+genoa-sullaway@users.noreply.github.com>
2023-12-28 14:59:18 -0800

    updated model back to N_sp no N_r
    estimates better, still having trouble with C_1 and Theta_1

--- scripts/Simulate_Data2.R
+++ scripts/Simulate_Data2.R
@@ -63,26 +63,24 @@ theta2 <- c(-0.2,0.1,-0.1) #relationship for simulated data
 # Simulate populations  ===================
 process_error_j <- matrix(nrow=n,ncol=1,rnorm(n*1,1,0.2))
 process_error_sp <- matrix(nrow=n,ncol=1,rnorm(n*1,50, 5))
-process_error_r <- matrix(nrow=n,ncol=1,rnorm(n*1,1,0.2))
+#process_error_r <- matrix(nrow=n,ncol=1,rnorm(n*1,1,0.2))
 
 N_eggs = matrix(nrow=n,ncol=n.pop,NA)
 kappa_fw =  matrix(nrow=n,ncol=n.pop,NA)
 N_j =  matrix(nrow=n,ncol=n.pop,NA)
 kappa_sp =  matrix(nrow=n,ncol=n.pop,NA)
 
-N_r <-matrix(nrow=n, ncol=pops, NA)
+#N_r <-matrix(nrow=n, ncol=pops, NA)
 
-mean.spawn <- c(mean(yukon_spring$Escapement), 
-                mean(yukon_fall$Estimated_Run), 
-                mean(kusko$pred_N_est)) #mean spawners for each population
+N_sp = matrix(nrow=n, ncol=pops, NA)
+mean.spawn <- c(mean(yukon_spring$Escapement), mean(yukon_fall$Estimated_Run), mean(kusko$pred_N_est)) #mean spawners for each population
+N_sp[1,] <- mean.spawn
+
+# N_sp <- matrix(nrow=n, ncol=pops, 
+#         data= c(rnorm(n, mean.spawn[1], mean(process_error_sp)), #simulating a unique number of spawners for each population
+#                 rnorm(n, mean.spawn[2], mean(process_error_sp)),
+#                 rnorm(n, mean.spawn[3], mean(process_error_sp))))
  
-N_sp <- matrix(nrow=n, ncol=pops, 
-        data= c(rnorm(n, mean.spawn[1], process_error_sp), #simulating a unique number of spawners for each population
-                rnorm(n, mean.spawn[2], mean(process_error_sp)),
-                rnorm(n, mean.spawn[3], mean(process_error_sp))))
-plot(N_sp[,1])
-plot(N_sp[,2])
-plot(N_sp[,3])
 
 p_1 =  matrix(nrow=n,ncol=n.pop,NA)
 p_2 =  matrix(nrow=n,ncol=n.pop,NA)
@@ -100,29 +98,32 @@ for(p in 1:n.pop) {
   N_j[i,p] = (N_eggs[i,p]*kappa_fw[i,p]) #+ obs_error_j[i]
   
   kappa_sp[i,p] <- (p_2[i,p])/(1 + ((p_2[i,p]*N_j[i,p])/c_2[p]))  
-  N_r[i,p] = (N_j[i,p]*kappa_sp[i,p])# + obs_error_sp[i]  
+  N_sp[i,p] = (N_j[i,p]*kappa_sp[i,p])# + obs_error_sp[i]  
     }
   }
 
 N_j[1,] = colMeans(N_j[2:n,]) #mean(N_j[2:n,1:n.pop]) # just so i don't get yelled at about NA's later down the road 
 
 N_j_sim <-  matrix(nrow=n,ncol=n.pop,NA)
-N_r_sim <-  matrix(nrow=n,ncol=n.pop,NA)
+N_sp_sim <-  matrix(nrow=n,ncol=n.pop,NA)
+#N_r_sim <-  matrix(nrow=n,ncol=n.pop,NA)
+
 
 for (p in 1:n.pop) {
 N_j_sim[,p] = rlnorm(n, log(N_j[2:n,p]), process_error_j[2:n])
-N_r_sim[,p] = rlnorm(n, log(N_r[2:n,p]), process_error_r[2:n])
+#N_r_sim[,p] = rlnorm(n, log(N_r[2:n,p]), process_error_r[2:n])
+N_sp_sim[,p] = rlnorm(n, log(N_sp[2:n,p]), process_error_sp[2:n])
 } 
 
 sigma_j_obs<-c(sd(log(N_j_sim)[6:n,1]),sd(log(N_j_sim)[6:n,2]),sd(log(N_j_sim)[6:n,3]))  #[6:n])
-sigma_sp_obs<-c(sd(log(N_sp)[6:n,1]),sd(log(N_sp)[6:n,2]),sd(log(N_sp)[6:n,3]))
-sigma_r_obs<-c(sd(log(N_r_sim)[6:n,1]),sd(log(N_r_sim)[6:n,2]),sd(log(N_r_sim)[6:n,3]))
+sigma_sp_obs<-c(sd(log(N_sp_sim)[6:n,1]),sd(log(N_sp_sim)[6:n,2]),sd(log(N_sp_sim)[6:n,3]))
+#sigma_r_obs<-c(sd(log(N_r_sim)[6:n,1]),sd(log(N_r_sim)[6:n,2]),sd(log(N_r_sim)[6:n,3]))
 
 dat_sim <- list(Population = population, Year = year,
-                 N_sp = N_sp,#[1:n,] ,
+                 N_sp = N_sp_sim,#[1:n,] ,
                  N_eggs = N_eggs[1:n,], 
                  N_j = N_j_sim, #[1:n,] , 
-                 N_r =  N_r_sim,
+                 #N_r =  N_r_sim,
                  p_1 = p_1, p_2=p_2, 
                  c_1=c_1, c_2=c_2,
                  kappa_sp=kappa_sp[1:n,],
@@ -131,15 +132,13 @@ dat_sim <- list(Population = population, Year = year,
 
 saveRDS(dat_sim , "data/Simulated_DatBH.RDS")
  
-plot(log(N_r[,1]))
+#plot(log(N_r[,1]))
 plot(N_j[,1])
 
-plot(N_sp[,1])
-plot(N_sp[,2])
-plot(N_sp[,3])
-# plot(N_j_sim) 
-# plot(N_sp_sim) 
-# 
+plot(N_sp[2:n,1])
+plot(N_sp[2:n,2])
+plot(N_sp[2:n,3])
+
 # hist(log(N_j_sim[6:n]))
 # hist(log(N_sp_sim[6:n]))
 # 
