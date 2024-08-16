02c09478b6df913c5504014991637fff3392b92a
Genoa Sullaway <42249501+genoa-sullaway@users.noreply.github.com>
2024-08-16 10:33:11 -0700

    not a great run - some stuff reverted to prior behaviour, need to revisit
    fullness index has a strong negative correlation with NBS SST and large zoop. so remove SST??
    
    - another though is to move fullness to imapcting marine survival not juvenile survival....

--- scripts/Covariate_multicolinearity_test.R
+++ scripts/Covariate_multicolinearity_test.R
@@ -1,8 +1,37 @@
-
 library(GGally)
 library(here)
 library(tidyverse)
 
+year_min = 2001
+year_max_cal = 2020
+year_max_brood = 2017
+
+# load more recent covariates ========================
+stage_a_cov <- read_csv("data/processed_covariates/stage_a_all.csv") %>%
+  dplyr::mutate(yukon_mean_discharge = as.numeric(scale(yukon_mean_discharge)),
+                SST_CDD_NBS = as.numeric(scale(SST_CDD_NBS)),
+                full_index = as.numeric(scale(full_index))) %>%
+  # zoop are already mean scaled
+  dplyr::rename(cal_year = Year) %>% 
+  dplyr::mutate(brood_year = cal_year-1) %>% 
+  filter(brood_year >= year_min, 
+         brood_year <= year_max_brood) %>%
+  dplyr::select(SST_CDD_NBS,# yukon_mean_discharge,
+                Large_zoop,
+                Cnideria,
+                full_index) 
+
+
+X_a<-cov_a[,2:8]
+colinearity_a <-ggpairs(X_a)
+pdf("output/cova_a_plot.pdf")
+print(colinearity_a)
+dev.off()
+
+
+
+
+# old ====================================
 # load covariates ==========
 cov_a <- read.csv("data/processed_covariates/stage_a_all.csv") %>%
   gather(2:8, key = "key", value = "value") %>% 
