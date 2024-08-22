63b0e03f2c150fb9962fbe8c5f564a18057e0553
Genoa Sullaway <42249501+genoa-sullaway@users.noreply.github.com>
2024-08-22 15:42:40 -0700

    send hood canal plots to tim

--- scripts/Hood_canal_CTDFish_comparison.R
+++
@@ -1,5 +0,0 @@
-library(here)
-library(tidyverse)
-
-# here : 
-#compare the locations where herring and hake were sampled in our hood canal work, compared to the full water column profiles.  That's a bit tricker, because it would have to be done on a tow-by-tow basis and it would be largely "presence only" data because of the way we sampled.  
